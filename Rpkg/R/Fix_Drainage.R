96004d86d47d27edf0c950ff22d5bc9494a9bf45
Laura Condon <lecondon@email.arizona.edu>
2019-08-18 20:08:34 -0700

    Adding river smoothing functions

---
+++ Rpkg/R/Fix_Drainage.R
@@ -0,0 +1,86 @@
+#'Walk upstream from a point ensuring DEM is increasing by a minum epsilon
+#'
+#'This walks upstream from a point following a flow direction file and checks that the elevation upstream
+#'is greater than or equal to the elvation at the point + epsilon
+#'Once the function reaches a point where upstream cells pass it stops 
+#'NOTE: this is only processing the immediate neigborhood and does not recurse over the entire domain, 
+#'For example if you did the entire overal prioirty flow processing with an espilon of 0 then ran this
+#'functon starting at a river point with an epsilon of 0.1 this function will traverse upstream from the 
+#'river bottom checking that every cell conneting to the river cell is higher by at least this ammout but 
+#'once it reaches a point where every connected cell passes this test it will stop. Therefore there could
+#'still be locations higher up on the hillslope with the original epsilon of zero still.  This  is on purpose
+#'and this script is not intended to gobally ensure a given epsilon. 
+
+#' @inheritParams CalcSubbasins
+#' @inheritParams RiverSmooth
+#' @export
+#' 
+FixDrainage=function(dem, direction, mask, epsilon, startpoint, d4=c(1,2,3,4)){
+  #D4 neighbors
+  #Rows: down, left top right
+  #Colums: (1)deltax, (2)deltay, direction number if you are waking upstream
+  ku=matrix(0, nrow=4, ncol=3) 
+  ku[,1]=c(0,1,0,-1)
+  ku[,2]=c(1,0,-1,0)
+  ku[,3]=c(1, 2, 3, 4)  
+  
+  #renumber the directions to 1=down, 2=left, 3=up, 4=right if a different numbering scheme was used
+  dir2=direction
+  nx=dim(direction)[1]
+  ny=dim(direction)[2]
+  if(d4[1]!=1){dir2[which(direction==d4[1])]=1}
+  if(d4[2]!=2){dir2[which(direction==d4[2])]=2}
+  if(d4[3]!=3){dir2[which(direction==d4[3])]=3}
+  if(d4[4]!=4){dir2[which(direction==d4[4])]=4}
+  
+  #initializing 
+  marked=matrix(0, nrow=nx, ncol=ny)
+  queue=cbind(startpoint[1],startpoint[2])
+  active=TRUE
+  dem2=dem
+  
+  while(active==T){
+    indx=queue[1,1]
+    indy=queue[1,2]
+    #print(paste("HERE", indx, indy))
+    queuetemp=NULL
+    #Loop over four directions check for non-stream neighbors pointing to this cell
+    for(d in 1:4){
+      tempx=indx+ku[d,1]
+      tempy=indy+ku[d,2]
+      #print(c(tempx,tempy, dir2[tempx,tempy]))
+      #if its point to the cell, is within the mask of cells to be processed, and has and epsilon < the threshold
+      if(tempx*tempy>0 & tempx<nx & tempy<ny){
+        if((d-dir2[tempx,tempy])==0 & mask[tempx,tempy]==1){
+            #print(paste("CHECKING:", tempx,tempy, dir2[tempx,tempy],round(dem[tempx,tempy],1), round(dem[indx,indy],1)))
+           if((dem2[tempx,tempy]-dem2[indx,indy])<epsilon){
+              dem2[tempx,tempy]=dem2[indx,indy]+epsilon
+              #print(paste("ADJUSTING:", tempx,tempy, "FROM", round(dem[tempx,tempy],1), "TO:", round(dem2[tempx,tempy],1)))
+              marked[tempx,tempy]=1
+              queuetemp=rbind(c(tempx,tempy),queuetemp)
+           } #end epsilon check
+        } #end drainge direction check
+      } #end domain check
+    }#end for d in 1:4
+    
+    #if cells were adjusted then add to the top of the queue replacing the cell that was just done
+    if(length(queuetemp>0)){
+      queue=rbind(queuetemp,queue[-1,])
+    }else{
+      #if no cells were adjsuted remove this cell from the queue and if its that last one you are done
+      if(nrow(queue)>1){
+        queue=queue[-1,]
+        if(length(queue)==2){
+          queue=matrix(queue, ncol=2, byrow=T)
+        } #fixing bug to keep q formated as a mstrix if it drops down to one row
+      }else{active=F}
+    }
+  } #end while active
+  #print(paste("Done! Start pont: ", startpoint[1], startpoint[2], ",", sum(marked), " cells adjusted", epsilon, sep=""))
+  
+  output_list=list("dem.adj"=dem2, "processed"=marked)
+  
+  return(output_list)
+
+}
+  
---
+++ Rpkg/R/River_Smoothing.R
@@ -0,0 +1,167 @@
+#'Apply smoothing to a DEM along a pre-defined stream network
+#'
+#'This function will apply a range of smoothing options to a DEM along a stream network. It requires pre-defined stream segments 
+#'and subbasins which can be obtained using the \code{\link{CalcSubbasins}} function.
+
+#' @inheritParams CalcSubbasins
+#' @param  river.summary A table summarizing the stream segments in a domain with the following. 
+#'  it shoudl have 1 row per stream segmentand the following 7 columns: (1) subbasin number, (2) x and (3) y index
+#'  of the upstream end of the stream segment, (4) x and (5) y index of the downstream eand of the river segement,
+#'  (6) the subbasin number for the downstream basin, -1 indicates a subbasin draining out of the domain
+#'  (7) drainage area of the subbasin 
+#'  @param river.segments A nx by ny matrix indicating the subbasin number for with for all grid cells on the river network 
+#'      ( (all cells not on the river network shoudl be 0)
+#'      
+#' @section River Methods:
+#' Add TEXT HERE
+#'
+#' @export
+#' 
+RiverSmooth=function(dem, direction, mask, river.summary, river.segments, epsilon=0.01, d4=c(1,2,3,4)){
+  ####################################################################
+  # PriorityFlow - Topographic Processing Toolkit for Hydrologic Models
+  # Copyright (C) 2018  Laura Condon (lecondon@email.arizona.edu)
+  # Contributors - Reed Maxwell (rmaxwell@mines.edu)
+  
+  # This program is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation version 3 of the License
+  
+  # This program is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+  
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <http://www.gnu.org/licenses/>
+  ####################################################################
+  ##TEMP
+  #dem=travHS$dem
+  #river.summary=subbasin$summary
+  #river.segments=subbasin$segments
+  #subasins=subbasin$subbasins
+
+  ##################
+  source("~/Dropbox/CONUS_Share/Topography_Testing/SmallTestDomain/Slope_processingNew/functions/Fix_Drainage.R", local=TRUE)
+  #D4 neighbors
+  #Rows: down, left top right
+  #Colums: (1)deltax, (2)deltay, direction number if you are waking (3)downstream, and  (4)upstream
+  kd=matrix(0, nrow=4, ncol=4) 
+  kd[,1]=c(0,-1,0,1)
+  kd[,2]=c(-1,0,1,0)
+  kd[,3]=c(1, 2, 3, 4)  
+  kd[,3]=c(3, 3, 3, 2)  
+  
+  if(missing(mask)){mask=matrix(1, nrow=nx, ncol=ny)} #default to processing everything
+
+  #renumber the directions to 1=down, 2=left, 3=up, 4=right if a different numbering scheme was used
+  dir2=direction
+  nx=dim(direction)[1]
+  ny=dim(direction)[2]
+  if(d4[1]!=1){dir2[which(direction==d4[1])]=1}
+  if(d4[2]!=2){dir2[which(direction==d4[2])]=2}
+  if(d4[3]!=3){dir2[which(direction==d4[3])]=3}
+  if(d4[4]!=4){dir2[which(direction==d4[4])]=4}
+  
+  #setup a river list 
+  nriver=nrow(river.summary)
+  marked.segments=rep(0,nriver) #marker for keeping track of which reaches are processed
+  marked.matrix=matrix(0, ncol=ny, nrow=nx)
+  
+  #make a mask of the hillslope cells
+  hillmask=mask
+  hillmask[which(river.segments>0)]=0
+  
+  #First make a list of all the termial river reaches
+  queue=which(river.summary[,6]==(-1))
+  if(length(queue)>0){active=TRUE}else{print("No terminal river segments provided, not adjusting DEM")}
+  
+  #start a new dem
+  dem2=dem
+  
+  #get the length of every river segment
+  river.length=rep(0,max(river.summary[,1])) #this has to be as long as the maximum river index not the number of rivers becasue there can be river indeces that don't exist in the river summary after merges occur
+  for(i in 1:nx){
+    for(j in 1:ny){
+      rtemp=river.segments[i,j]
+      river.length[rtemp]=river.length[rtemp]+1
+    }
+  }
+  
+  #Loop over the river segments working upstream
+  #starting from every terminal segment (i.e. segments with a downstream river number of -1)
+  while(active==T){
+    indr=queue[1]
+    r=river.summary[indr,1] #river segment number
+    rdown=river.summary[indr,6]
+    
+    #find the top and bottom elevations of the current river segment
+    top=dem[river.summary[indr,2], river.summary[indr,3]]
+    #if its a terminal reach then the bottom elevation will be the bottom of the reach
+    if(rdown==(-1)){
+      bottom=dem[river.summary[indr,4], river.summary[indr,5]]
+    } else{
+    #if not then use the elevation downstream of the bottom point of the reach
+      bdir=dir2[river.summary[indr,4], river.summary[indr,5]]
+      bottom=dem[(river.summary[indr,4]+kd[bdir,1]), (river.summary[indr,5]+kd[bdir,2])]
+    }
+    #print(paste("River segment:",r))
+    #print(paste("Start:",river.summary[indr,2], river.summary[indr,3], round(top,1)))
+    #print(paste("End:",river.summary[indr,4], river.summary[indr,5], round(bottom,1)))
+
+    #walk from top to bottom smoothing out the river cells
+    temp=top
+    indx=river.summary[indr,2]
+    indy=river.summary[indr,3]
+    marked.matrix[indx,indy]=marked.matrix[indx,indy]+1
+    length=river.length[r]
+    if(river.segments[indx,indy]!=r){length=length+1} #add one to the river length if the top cell has a different river number (this happens with non-headwater segments)
+    if(length>1){delta=(top-bottom)/(length-1)}else{delta=0}
+    if(delta<0){
+      print(paste("ERROR: River top elevation<river bottom elevation for segment", r))
+      print("Applying a delta of 0 for this reach")
+      delta=0
+    }
+    if(length>1){
+      for(i in 2:length){
+        #print(i)
+        temp=temp-delta
+        #find the downstream point and adjust its elevation
+        dirtemp=dir2[indx,indy]
+        downindx=indx+kd[dirtemp,1]
+        downindy=indy+kd[dirtemp,2]
+        dem2[downindx,downindy]=temp
+        
+        #move to the dlownstream point
+        indx=downindx
+        indy=downindy
+        marked.matrix[indx,indy]=marked.matrix[indx,indy]+1
+        
+        #loop up the hillslope from the point and make sure everything drains
+        drainfix=FixDrainage(dem=dem2, direction=dir2, mask=hillmask, epsilon=epsilon, startpoint=c(indx,indy))
+        dem2=drainfix$dem.adj
+        #print(paste(indx, indy, round(temp)))
+      } #end for i in length river segment
+    } # end if length >1
+    
+    #mark this segement as done
+    marked.segments[indr]= marked.segments[indr]+1
+      
+    #Find all of the river segments that drain to this segment
+    uplist=which(river.summary[,6]==r)
+    #if there are upstream segments then add to the top of the Q overwriting the segment that
+    #was just done if not just remove the current segment from the queue
+    if(length(uplist>0)){queue=c(uplist,queue[-1])} else{queue=queue[-1]}
+    if(length(queue)==0){active=F}
+  } #end while active
+      
+output_list=list("dem.adj"=dem2, "processed"=marked.matrix)
+  
+return(output_list)
+} # End function
+  
+    
+    
+    
+   
+
