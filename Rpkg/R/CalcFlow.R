e6c7ca212e736e8be6b9029be8d6317404faa750
Laura Condon <lecondon@email.arizona.edu>
2020-03-25 17:41:42 -0700

    updating documentation with calc flow and adding pfb write function

--- Rpkg/R/CalcFlow.R
+++ Rpkg/R/CalcFlow.R
@@ -4,7 +4,7 @@
 #' @param file.path path where pressure and slope files are located, this is also where flow files will be written to
 #' @param run.name ParFlow run name this is used to read the pressure files assumign they are named with the
 #'         standard ParFlow naming convention (i.e. runname.out.press.00000.pfb)
-#' @param file.range range of file numbers to be processed (i.e c(0,10))
+#' @param file.nums list of file numbers to be processed
 #' @param slopex.file name of the x slopes file  (note this must be a pfb file)
 #' @param slopey.file  name of the y slopes file  (note this must be a pfb file)
 #' @param overland.type  type of overland flow used for ParFlow simulation. Choices are 
@@ -19,21 +19,24 @@
 #'       domain and [nx,ny] is the upper right
 #'       
 #'       
-CalcFlow=function(file.path, run.name, file.range, slopex.file,
+CalcFlow=function(file.path, run.name, file.nums, slopex.file,
                   slopey.file, overland.type, mannings, epsilon=1e-5,
                   dx=1, dy=1, mask){
   
   
 #Get the range of file number to be read in   
-file.nums=file.range[1]:file.range[2]
-nfile=length(filenums)
+nfile=length(file.nums)
 
 #make a zeros matrix for pmax calcs later
 zeros=matrix(0, nrow=nx, ncol=ny)
 
 #read in the slope files
-slopex=sprintf("%s/%s", file.path, slopex.file)
-slopey=sprintf("%s/%s", file.path, slopyx.file)
+fin.x=sprintf("%s/%s", file.path, slopex.file)
+slopex=readpfb(fin.x, verbose=F)
+fin.y=sprintf("%s/%s", file.path, slopey.file)
+slopey=readpfb(fin.y, verbose=F)
+slopex=slopex[,,1]
+slopey=slopey[,,1]
 nx=dim(slopex)[1]
 ny=dim(slopex)[2]
 
@@ -99,16 +102,40 @@ for(f in 1:nfile){
   ## Overland Kinematic
   ################################################
   } else if (overland.type=="OverlandKinematic"){
+    ####
+    #Repeat the slopes on the lower and left boundaries that are inside the domain but outside the mask
+    #find indices of all cells that are off the mask but have a neigbor to their right that is on the mask
+    fill.left=which((rbind(mask[2:nx,],rep(0,ny)) - mask[1:nx,]) ==1, arr.ind=T)
+    #get the indices of their neigbors to the right 
+    fill.left2=fill.left
+    fill.left2[,1]=fill.left[,1]+1
+    #pad the slopes to the left with their neigboring cells in the mask 
+    slopex[fill.left]=slopex[fill.left2]
+    
+    #find indices of all cells that are off the mask but have a neigbor above them that is on the mask
+    fill.down=which((cbind(mask[,2:ny],rep(0,nx)) - mask[,1:ny]) ==1, arr.ind=T)
+    #get the indices of their neigbors above
+    fill.down2=fill.down
+    fill.down2[,2]=fill.down[,2]+1
+    #pad the slopes to below  with their neigboring cells in the mask 
+    slopey[fill.down]=slopex[fill.down2]
+    
+    
+    ####
+    #calculate the slope magnitude
     sfmag=pmax((slopex^2+slopey^2)^0.5, epsilon)
-  
+    
+    
+    ###
     #For OverlandKinematic slopes are face centered and calculated across the upper and right boundaries
     # (i.e. Z[i+1]-Z[i])
     # For cells on the lower and left boundaries its assumed that the slopes repeat 
     # (i.e. repeating the upper and right face boundary for the lower and left for these border cells)
     slopex.pad=rbind(slopex[1,], slopex)
     slopey.pad=cbind(slopey[,1], slopey)
-  
-    #upwind the pressure - Note this is for the north and east face of all cells
+    
+    ####
+    # upwind the pressure - Note this is for the north and east face of all cells
     # The slopes are calculated across these boundaries so the upper boundary is included in these 
     # calculations and the lower and righ boundary of the domain will be added later
     pupwindx=pmax(sign(slopex)*rbind(ptop[2:(nx),], rep(0,ny)),0) +
@@ -116,16 +143,19 @@ for(f in 1:nfile){
     pupwindy=pmax(sign(slopey) * cbind(ptop[,2:ny], rep(0,nx)),0) +
            pmax(-sign(slopey) * ptop[, 1:ny],0)
   
+    ###
     # Calculate fluxes across east and north faces
     # First the x direction
     qeast = -slopex/(sfmag^0.5*mannings) * pupwindx^(5/3) *dy #Units should be l3/t
     qnorth = -slopey/(sfmag^0.5*mannings) * pupwindy^(5/3) *dx #Units should be l3/t
   
+    ###
     #Fix the lower x boundary 
     # Use the slopes of the first column with the pressures for cell i 
     qleft=-slopex[1,]/(sfmag[1,]^0.5*mannings)* (pmax(sign(slopex[1,])*ptop[1,],0))^(5/3) * dy
     qeast=rbind(qleft,qeast)
   
+    ###
     # Fix the lower y boundary 
     # Use the slopes of the bottom row with the pressures for cell j
     qbottom=-slopey[,1]/(sfmag[,1]^0.5*mannings)* (pmax(sign(slopey[,1])*ptop[,1],0))^(5/3) * dx
@@ -143,16 +173,16 @@ for(f in 1:nfile){
           pmax(qnorth[,2:(ny+1)],zeros) + pmax(-qnorth[, 1:ny], zeros)
 
   #Write the outputs
-  outflow.file=sprintf("%s/%s.out.outflow.%05d.pfb", file.path, run.name, file.number)
+  outflow.file=sprintf("%s/%s.out.outflow.%05d.pfb", file.path, run.name, fn)
   writepfb(outflow, outflow.file, dx, dy, 1)
 
   #Write the outputs
-  qeast.file=sprintf("%s/%s.out.q_east.%05d.pfb", file.path, run.name, file.number)
+  qeast.file=sprintf("%s/%s.out.q_east.%05d.pfb", file.path, run.name, fn)
   writepfb(qeast, qeast.file, dx, dy, 1)
 
   #Write the outputs
-  qnorth.file=sprintf("%s/%s.out.q_east.%05d.pfb", file.path, run.name, file.number)
-  writepfb(qnorth, qeast.north, dx, dy, 1)
+  qnorth.file=sprintf("%s/%s.out.q_east.%05d.pfb", file.path, run.name, fn)
+  writepfb(qnorth, qnorth.file, dx, dy, 1)
 
 } #end for f
 #output_list=list("qeast"=qeast, "qnorth"=qnorth, "outflow"=outflow)
